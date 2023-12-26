ceb66ea5d7c401be6445b6a9bc321683cabe49eb
Atlon1 <maciej.szajstek@gmail.com>
2023-12-27 00:34:55 +0100

    Billboard added to frontend page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboards from "@/actions/get-billboards";
 
-const HomePage = () => {
+export const revalidate = 0
+
+const HomePage = async () => {
+
+    const billboard = await getBillboards('f01328b4-796f-4a60-a965-b3062fdd3d77')
+
+
     return (
-        <div>
-            hello store
-        </div>
+        <Container>
+            <div className='space-y-10 pb-10'>
+                <Billboard data={billboard}/>
+            </div>
+        </Container>
     );
 };
 
