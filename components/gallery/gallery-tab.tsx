3c573bfa49664eb5153c1a9c7f1f649a03b86cc9
Atlon1 <maciej.szajstek@gmail.com>
2023-12-28 02:08:05 +0100

    gallery added

--- components/gallery/gallery-tab.tsx
+++ components/gallery/gallery-tab.tsx
@@ -5,15 +5,30 @@ import {cn} from "@/lib/utlis";
 import {Image as ImageType} from "@/types";
 
 
-
 interface GalleryTabProps {
-image: ImageType
+    image: ImageType
 }
+
 const GalleryTab: React.FC<GalleryTabProps> = ({image}) => {
     return (
-        <div>
-            GalleryTab
-        </div>
+        <Tab className='relative flex aspect-square cursor-pointer items-center justify-center rounded-md bg-white'>
+            {({selected}) => (
+                <div>
+                    <span className='absolute h-full w-full aspect-square inset-0 overflow-hidden rounded-md'>
+                        <Image
+                            fill
+                            src={image.url}
+                            alt='Image'
+                            className='object-cover object-center'
+                        />
+                    </span>
+                    <span className={cn(
+                        'absolute inset-0 rounded-md ring-2 ring-offset-2',
+                        selected ? 'ring-black' : 'ring-transparent'
+                    )}/>
+                </div>
+            )}
+        </Tab>
     );
 };
 
--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -14,7 +14,7 @@ interface GalleryProps {
 
 const Gallery: React.FC<GalleryProps> = ({images}) => {
     return (
-        <Tab.Group as="div" className="flex flex-col gap-4">
+        <Tab.Group as="div" className="flex flex-col-reverse">
             <div className='mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none'>
                 <Tab.List className='grid grid-cols-4 gap-6'>
                     {images.map((image) => (
@@ -22,6 +22,20 @@ const Gallery: React.FC<GalleryProps> = ({images}) => {
                     ))}
                 </Tab.List>
             </div>
+            <Tab.Panels className='aspect-square w-full'>
+                {images.map((image)=>(
+                    <Tab.Panel key={image.id}>
+                        <div className='aspect-square relative h-full w-full sm:rounded-lg overflow-hidden'>
+                            <Image
+                                fill
+                                src={image.url}
+                                alt="Image"
+                                className='object-cover object-center'
+                            />
+                        </div>
+                    </Tab.Panel>
+                ))}
+            </Tab.Panels>
         </Tab.Group>
     );
 };
