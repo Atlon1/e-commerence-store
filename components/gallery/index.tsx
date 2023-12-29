19187980a39df102508380f7a5858bfed810bbb6
Atlon1 <maciej.szajstek@gmail.com>
2023-12-29 13:38:57 +0100

    added info to product cart

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,36 +1,37 @@
 "use client";
 
-import React from 'react';
+import NextImage from "next/image";
+import { Tab } from "@headlessui/react";
 
-import Image from "next/image";
-import {Tab} from "@headlessui/react";
+import { Image } from "@/types";
 
-import {Image as ImageType} from "@/types";
-import GalleryTab from "@/components/gallery/gallery-tab";
+import GalleryTab from "./gallery-tab";
 
 interface GalleryProps {
-    images: ImageType[]
+    images: Image[];
 }
 
-const Gallery: React.FC<GalleryProps> = ({images}) => {
+const Gallery: React.FC<GalleryProps> = ({
+                                             images = []
+                                         }) => {
     return (
         <Tab.Group as="div" className="flex flex-col-reverse">
-            <div className='mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none'>
-                <Tab.List className='grid grid-cols-4 gap-6'>
+            <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+                <Tab.List className="grid grid-cols-4 gap-6">
                     {images.map((image) => (
-                        <GalleryTab key={image.id} image={image}/>
+                        <GalleryTab key={image.id} image={image} />
                     ))}
                 </Tab.List>
             </div>
-            <Tab.Panels className='aspect-square w-full'>
-                {images.map((image)=>(
+            <Tab.Panels className="aspect-square w-full">
+                {images.map((image) => (
                     <Tab.Panel key={image.id}>
-                        <div className='aspect-square relative h-full w-full sm:rounded-lg overflow-hidden'>
-                            <Image
+                        <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+                            <NextImage
                                 fill
                                 src={image.url}
                                 alt="Image"
-                                className='object-cover object-center'
+                                className="object-cover object-center"
                             />
                         </div>
                     </Tab.Panel>
@@ -38,6 +39,6 @@ const Gallery: React.FC<GalleryProps> = ({images}) => {
             </Tab.Panels>
         </Tab.Group>
     );
-};
+}
 
 export default Gallery;
